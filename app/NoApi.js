e8421ceacdad2844b8b3ff48e703bcd9e83d6504
snaterlicious <github@snater.com>
2014-07-14 15:33:32 +0200

    Sanitized NoApi.js
    Added documentation, slimmed down functionality.

--- app/NoApi.js
+++ app/NoApi.js
@@ -12,116 +12,115 @@ define( [
 	'app/LicenceStore',
 	'app/LICENCES'
 ],
-	function(
-		$,
-		messages,
-		ApplicationError,
-		Asset,
-		Author,
-		ImageInfo,
-		LicenceStore,
-		LICENCES
-		) {
-		'use strict';
+function(
+	$,
+	messages,
+	ApplicationError,
+	Asset,
+	Author,
+	ImageInfo,
+	LicenceStore,
+	LICENCES
+) {
+'use strict';
 
-		/**
-		 * Handler for simple file names.
-		 * @constructor
-		 */
-		var NoApi = function() {
+/**
+ * Handler for simple file names.
+ * @constructor
+ */
+var NoApi = function() {};
 
-		};
+$.extend( NoApi.prototype, {
+	/**
+	 * Generates an Asset object for a specific filename.
+	 *
+	 * @param {string} filename
+	 * @return {Object} jQuery Promise
+	 *         Resolved parameters:
+	 *         - {Asset}
+	 *         Rejected parameters:
+	 *         - {ApplicationError}
+	 */
+	getAsset: function( filename ) {
+		var self = this,
+			deferred = $.Deferred();
 
-		$.extend( NoApi.prototype, {
-			/**
-			 * Generates an Asset object for a specific filename.
-			 *
-			 * @param {string} filename
-			 * @return {Object} jQuery Promise
-			 *         Resolved parameters:
-			 *         - {Asset}
-			 *         Rejected parameters:
-			 *         - {AjaxError}
-			 */
-			getAsset: function( filename ) {
-				var self = this,
-					deferred = $.Deferred();
+		this.getImageInfo( filename )
+		.done( function() {
+			var licence = new LicenceStore( LICENCES ).detectLicence( 'unknown' ),
+				author = new Author( $( document.createTextNode( messages['author-undefined'] ) ) );
 
-				this._getFile( filename )
-					.done( function() {
-						var licence = new LicenceStore( LICENCES ).detectLicence( 'unknown' );
-						var anchorTag = $( '<a/>' );
-						var author = new Author(
-							anchorTag.append(
-								document.createTextNode( messages['author-undefined'] )
-							)
-						);
-
-						var asset = new Asset(
-							filename,
-							messages['file-untitled'],
-							'mediatype',
-							licence,
-							self,
-							{
-								authors: [ author ]
-							},
-							''
-						);
-						deferred.resolve( asset );
-					} )
-					.fail( function( error ) {
-						deferred.reject( error );
-					} );
-
-				return deferred.promise();
-			},
-
-			getImageInfo: function( filename, imageSize, wikiUrl ) {
-				var deferred = $.Deferred();
-
-				var $img = $( '<img/>' )
-					.css( 'display', 'none' )
-					.attr( 'src', filename )
-					.load( function() {
-						$( 'body' ).append( $img );
-						deferred.resolve( new ImageInfo( filename, filename,
-							{
-								url: filename,
-								width: $img.width(),
-								height: $img.height()
-							}
-						));
-					});
-
-				return deferred.promise();
-			},
-
-			getLicenceStore: function() {
-				return new LicenceStore();
-			},
-
-			_getFile: function( filename ) {
-				var deferred = $.Deferred();
-				var $img = $( '<img/>' )
-					.attr( 'src', filename )
-					.css( 'display', 'none' )
-					.error( function() {
-						deferred.reject( new ApplicationError( 'url-invalid' ) );
-					})
-					.load( function() {
-						$('body').append($img);
-						deferred.resolve( filename );
-					});
-				return deferred.promise();
-			},
-
-			getDefaultUrl: function() {
-				return '';
-			}
-
+			var asset = new Asset(
+				filename,
+				messages['file-untitled'],
+				'mediatype',
+				licence,
+				self,
+				{
+					authors: [author]
+				},
+				''
+			);
+			deferred.resolve( asset );
+		} )
+		.fail( function( error ) {
+			deferred.reject( error );
 		} );
 
-		return NoApi;
+		return deferred.promise();
+	},
 
-	} );
+	/**
+	 * @return {LicenceStore|null}
+	 */
+	getLicenceStore: function() {
+		return new LicenceStore();
+	},
+
+	/**
+	 * Retrieves image information for a specific file.
+	 *
+	 * @param {string} filename
+	 * @return {Object} jQuery Promise
+	 *         Resolved parameters:
+	 *         - {ImageInfo}
+	 *         Rejected parameters:
+	 *         - {ApplicationError}
+	 */
+	getImageInfo: function( filename ) {
+		var deferred = $.Deferred();
+
+		var $img = $( '<img/>' )
+			.css( 'display', 'none' )
+			.attr( 'src', filename )
+			.error( function() {
+				deferred.reject( new ApplicationError( 'url-invalid' ) );
+				$img.remove();
+			} )
+			.load( function() {
+				$( 'body' ).append( $img );
+
+				deferred.resolve( new ImageInfo( filename, filename, {
+					url: filename,
+					width: $img.width(),
+					height: $img.height()
+				} ) );
+
+				$img.remove();
+			} );
+
+		return deferred.promise();
+	},
+
+	/**
+	 * @return {string}
+	 */
+	getDefaultUrl: function() {
+		return '';
+	}
+
+} );
+
+return NoApi;
+
+} );
