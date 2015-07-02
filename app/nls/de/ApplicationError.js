7a1407f4514c7db11a495be78880c41d8da687f1
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-07-02 17:19:32 +0200

    Show an error message if no licence template has been recognized

--- app/nls/de/ApplicationError.js
+++ app/nls/de/ApplicationError.js
@@ -2,5 +2,6 @@ define( {
 	'*': 'Ein unvorhergesehener Fehler ist aufgetreten.',
 	'mediatype-unsupported': 'Der Medientyp der angegebenen Datei wird von dieser Applikation momentan leider nicht unterstützt.',
 	'licence-unsupported': 'Leider wird die ermittelte Lizenz des Bildes von dieser Anwendung nicht unterstützt.',
-	'url-invalid': 'Die angegebene Internetadresse konnte nicht verarbeitet werden.'
+	'url-invalid': 'Die angegebene Internetadresse konnte nicht verarbeitet werden.',
+	'licence-not-recognized': 'Es tut uns leid, aber dieses Bild wird aufgrund des Lizenzformats derzeit noch nicht unterstützt.'
 } );
