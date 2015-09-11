4242f1d84c915a0b7eeec6addcc132fa303203f4
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-09-11 17:17:31 +0200

    Make jscs happy.

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,9 +1,9 @@
 ( function( $ ) {
-  $( '#how-it-works-button' ).click( function () {
-    $( '#how-it-works-screen' ).slideDown();
-  } );
+	$( '#how-it-works-button' ).click( function() {
+		$( '#how-it-works-screen' ).slideDown();
+	} );
 
-  $( '#how-it-works-screen .close' ).click( function () {
-    $( '#how-it-works-screen' ).slideUp();
-  } );
+	$( '#how-it-works-screen .close' ).click( function() {
+		$( '#how-it-works-screen' ).slideUp();
+	} );
 }( jQuery ) );
