f9954f4f6ba35c87fb58bf89219c3cbb4b0c48de
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-09-29 13:14:45 +0200

    Add feedback feature.

--- backend/config.example.php
+++ backend/config.example.php
@@ -2,4 +2,5 @@
 return [
   'debug' => true,
   'swiftmailer.transport' => Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs'),
+  'feedback_email' => 'mail@example.com', // the email address that receives the feedback
 ];
--- backend/src/Actions/FeedbackAction.php
+++ backend/src/Actions/FeedbackAction.php
@@ -2,11 +2,55 @@
 namespace AttributionGenerator\Actions;
 
 class FeedbackAction {
-  function __construct( \Silex\Application $app ) {
+	private $app;
+	private $errors = [];
 
-  }
+	function __construct( \Silex\Application $app ) {
+		$this->app = $app;
 
-  public function getResponse() {
-    return '';
-  }
+		if ( $this->requestValid( $app['request'] ) ) {
+			$this->sendMail(
+				$app['request']->get( 'name' ),
+				$app['request']->get( 'feedback' )
+			);
+		}
+	}
+
+	private function requestValid( \Symfony\Component\HttpFoundation\Request $request ) {
+		if ( empty( $request->get( 'name' ) ) || empty( $request->get( 'feedback' ) ) ) {
+			$this->errors[] = 'Alle Felder des Feedback-Formulars müssen ausgefüllt werden.';
+			return false;
+		}
+
+		return true;
+	}
+
+	private function sendMail( $sender, $text ) {
+		$message = \Swift_Message::newInstance()
+			->setSubject( '[AttributionGenerator] Feedback from ' . $sender )
+			->setFrom( [ 'noreply@attribution-generator.dev' ] )
+			->setTo( [ $this->app['config']['feedback_email'] ] )
+			->setBody( $text );
+
+		try {
+			$this->app['mailer']->send( $message );
+		} catch ( Exception $e ) {
+			$this->errors[] = 'Beim Senden der Nachricht ist ein Fehler aufgetreten. Bitte versuche es erneut.';
+		}
+	}
+
+	private function success() {
+		return [ 'message' => 'Vielen Dank für Dein Feedback!' ];
+	}
+
+	public function getResponse() {
+		if ( empty( $this->errors ) ) {
+			return json_encode( $this->success(), JSON_UNESCAPED_UNICODE );
+		} else {
+			return json_encode(
+				[ 'errors' => $this->errors ],
+				JSON_UNESCAPED_UNICODE
+			);
+		}
+	}
 }
