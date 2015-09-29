5245a9b68bb5b21e6bd23ed64935087eaf2d6095
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-09-29 13:27:17 +0200

    Fix some whitespace.

--- backend/app.php
+++ backend/app.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['config'] = require __DIR__ . '/config.php';
--- backend/tests/ApplicationTest.php
+++ backend/tests/ApplicationTest.php
@@ -3,13 +3,13 @@
 use Silex\WebTestCase;
 
 class ApplicationTest extends WebTestCase {
-  public function createApplication() {
-    return require __DIR__ . '/../app.php';
-  }
+	public function createApplication() {
+		return require __DIR__ . '/../app.php';
+	}
 
-  public function testAppRunning() {
-    $client = $this->createClient();
-    $client->request('GET', '/');
-    $this->assertTrue($client->getResponse()->isOk());
-  }
+	public function testAppRunning() {
+		$client = $this->createClient();
+		$client->request( 'GET', '/' );
+		$this->assertTrue( $client->getResponse()->isOk() );
+	}
 }
