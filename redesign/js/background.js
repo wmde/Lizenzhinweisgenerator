99aaa43b6fba558b5155c44699a09c6d9de4b81b
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-10-05 12:18:33 +0200

    Do not load background images asynchronously.

--- redesign/js/background.js
+++ redesign/js/background.js
@@ -1,18 +1,16 @@
 'use strict';
 
-var $ = require( 'jquery' );
+var $ = require( 'jquery' ),
+	images = require( '../data/background_images.json' );
 
 var daysSince1970 = Math.round(
 	( new Date() ).getTime() / ( 60 * 1000 * 60 * 24 )
 );
 
-$.getJSON( 'data/background_images.json' )
-	.done( function( images ) {
-		var imageNr = daysSince1970 % images.length;
+var imageNr = daysSince1970 % images.length;
 
-		$( '#attribution' ).html( images[ imageNr ].attribution );
-		$( '#landing-screen' ).css(
-			'background-image',
-			'url(' + images[ imageNr ].url + ')'
-		);
-	} );
+$( '#attribution' ).html( images[ imageNr ].attribution );
+$( '#landing-screen' ).css(
+	'background-image',
+	'url(' + images[ imageNr ].url + ')'
+);
