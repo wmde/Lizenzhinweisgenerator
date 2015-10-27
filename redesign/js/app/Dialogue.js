8b4640c96d50c51e38149b3c6b5c4dc59a298be6
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-10-27 16:34:19 +0100

    Make Dialogue contain DialogueSteps.

--- redesign/js/app/Dialogue.js
+++ redesign/js/app/Dialogue.js
@@ -5,4 +5,18 @@ var $ = require( 'jquery' );
 var Dialogue = function() {
 };
 
+$.extend( Dialogue.prototype, {
+	/**
+	 * @type {DialogueStep[]}
+	 */
+	_steps: [],
+
+	/**
+	 * @param {DialogueStep} step
+	 */
+	addStep: function( step ) {
+		this._steps.push( step );
+	}
+} );
+
 module.exports = Dialogue;
--- redesign/tests/app/Dialogue.tests.js
+++ redesign/tests/app/Dialogue.tests.js
@@ -2,4 +2,16 @@
 
 QUnit.module( 'Dialogue' );
 
-var Dialogue = require( '../../js/app/Dialogue' );
+var Dialogue = require( '../../js/app/Dialogue' ),
+	DialogueStep = require( '../../js/app/DialogueStep' );
+
+QUnit.test( 'can contain steps', function( assert ) {
+	var dialogue = new Dialogue,
+		step1 = new DialogueStep( 'test1' ),
+		step2 = new DialogueStep( 'test2' );
+
+	dialogue.addStep( step1 );
+	dialogue.addStep( step2 );
+
+	assert.equal( dialogue._steps.length, 2 );
+} );
