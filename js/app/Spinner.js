f76cc38e36b4007372152b5fb21d656dbfda2145
Jakob Warkotsch <j.warkotsch@gmail.com>
2016-01-06 13:52:40 +0100

    Add loading spinner class.

---
+++ js/app/Spinner.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var $ = require( 'jquery' );
+
+var Spinner = function( $node ) {
+	this._$node = $node;
+};
+
+$.extend( Spinner.prototype, {
+	/**
+	 * @type {jQuery}
+	 */
+	_$node: null,
+
+	/**
+	 * Adds a spinner with `size` px height to a node.
+	 *
+	 * @param {int} size
+	 */
+	add: function() {
+		if( this._$node.find( '.ag-spinner' ).length !== 0 ) {
+			return;
+		}
+
+		this._$node.prepend(
+			'<div class="ag-spinner"><i class="glyphicon spin-icon glyphicon-refresh"></i></div>'
+		);
+	},
+
+	remove: function() {
+		this._$node.find( '.ag-spinner' ).remove();
+	}
+} );
+
+module.exports = Spinner;
