f6d2511d78d6e8ed69d7f82361d14924034ef0a0
addshore <addshorewiki@gmail.com>
2016-04-27 09:38:16 +0100

    Allow switching language using lang url param
    Currently if a language is selected and i18n messages
    are missing the i18n code will be displayed instead
    
    If no language is passed a default of de is used.

--- js/app/Messages.js
+++ js/app/Messages.js
@@ -3,4 +3,24 @@
 var Polyglot = require( 'node-polyglot' ),
 	i18n = require( '../i18n.json' );
 
-module.exports = new Polyglot( { phrases: i18n[ 'de' ] } );
+var getParam = function getUrlParameter( sParam ) {
+	var sPageURL = decodeURIComponent( window.location.search.substring( 1 ) ),
+		sURLVariables = sPageURL.split( '&' ),
+		sParameterName,
+		i;
+
+	for( i = 0; i < sURLVariables.length; i++ ) {
+		sParameterName = sURLVariables[i].split( '=' );
+
+		if( sParameterName[0] === sParam ) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
+};
+
+var lang = getParam( 'lang' );
+if( lang === undefined ) {
+	lang = 'de';
+}
+
+module.exports = new Polyglot( { phrases: i18n[ lang ] } );
