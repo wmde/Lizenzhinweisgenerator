16c82898e953b24d65fa5bfd445aa48294a3e476
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-05-17 10:38:06 +0200

    Switch all URLs in InputHandler's "local" tests to use HTTPS

--- tests/app/InputHandler.local.tests.js
+++ tests/app/InputHandler.local.tests.js
@@ -18,13 +18,13 @@ var api = new LocalApi( 'fixtures' );
 var testCases = [
 	{
 		input: [
-			'http://commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg',
+			'https://commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg',
 			'commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg',
-			'http://commons.wikimedia.org/w/index.php?title=File:Helene_Fischer_2010.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/8/84/Helene_Fischer_2010.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Helene_Fischer_2010.jpg/171px-Helene_Fischer_2010.jpg',
+			'https://commons.wikimedia.org/w/index.php?title=File:Helene_Fischer_2010.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/8/84/Helene_Fischer_2010.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Helene_Fischer_2010.jpg/171px-Helene_Fischer_2010.jpg',
 			'upload.wikimedia.org/wikipedia/commons/thumb/8/84/Helene_Fischer_2010.jpg/171px-Helene_Fischer_2010.jpg',
-			'http://commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg#mediaviewer/File:Helene_Fischer_2010.jpg',
+			'https://commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg#mediaviewer/File:Helene_Fischer_2010.jpg',
 			'commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg?uselang=de',
 			'https://commons.wikimedia.org/w/index.php?title=File:Helene_Fischer_2010.jpg&uselang=de',
 			'commons.wikimedia.org/wiki/File:Helene_Fischer_2010.jpg?foo=bar',
@@ -41,7 +41,7 @@ var testCases = [
 		}
 	}, {
 		input: [
-			'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Le_Bistro/24_f%C3%A9vrier_2012#mediaviewer/Fichier:Helene_Fischer_2010.jpg'
+			'https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Le_Bistro/24_f%C3%A9vrier_2012#mediaviewer/Fichier:Helene_Fischer_2010.jpg'
 		],
 		expected: {
 			file: 'File:Helene_Fischer_2010.jpg',
@@ -49,12 +49,12 @@ var testCases = [
 		}
 	}, {
 		input: [
-			'http://commons.wikimedia.org/wiki/File:JapaneseToiletControlPanel.jpg',
-			'http://commons.wikimedia.org/w/index.php?title=File:JapaneseToiletControlPanel.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/e/e4/JapaneseToiletControlPanel.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/JapaneseToiletControlPanel.jpg/320px-JapaneseToiletControlPanel.jpg',
-			'http://commons.wikimedia.org/wiki/File:JapaneseToiletControlPanel.jpg#mediaviewer/File:JapaneseToiletControlPanel.jpg',
-			'http://commons.wikimedia.org/wiki/User:Chris_73/Gallery_001#mediaviewer/File:JapaneseToiletControlPanel.jpg',
+			'https://commons.wikimedia.org/wiki/File:JapaneseToiletControlPanel.jpg',
+			'https://commons.wikimedia.org/w/index.php?title=File:JapaneseToiletControlPanel.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/e/e4/JapaneseToiletControlPanel.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/JapaneseToiletControlPanel.jpg/320px-JapaneseToiletControlPanel.jpg',
+			'https://commons.wikimedia.org/wiki/File:JapaneseToiletControlPanel.jpg#mediaviewer/File:JapaneseToiletControlPanel.jpg',
+			'https://commons.wikimedia.org/wiki/User:Chris_73/Gallery_001#mediaviewer/File:JapaneseToiletControlPanel.jpg',
 			'https://commons.m.wikimedia.org/wiki/File:JapaneseToiletControlPanel.jpg',
 			'https://commons.m.wikimedia.org/w/index.php?title=File:JapaneseToiletControlPanel.jpg'
 		],
@@ -64,12 +64,12 @@ var testCases = [
 		}
 	}, {
 		input: [
-			'http://commons.wikimedia.org/wiki/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
-			'http://commons.wikimedia.org/w/index.php?title=File:Gerardus_t%27_Hooft_at_Harvard.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/f/f4/Gerardus_t%27_Hooft_at_Harvard.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Gerardus_t%27_Hooft_at_Harvard.jpg/180px-Gerardus_t%27_Hooft_at_Harvard.jpg',
-			'http://commons.wikimedia.org/wiki/File:Gerardus_t%27_Hooft_at_Harvard.jpg#mediaviewer/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
-			'http://commons.wikimedia.org/wiki/Gerardus_%27t_Hooft#mediaviewer/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://commons.wikimedia.org/wiki/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://commons.wikimedia.org/w/index.php?title=File:Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/f/f4/Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Gerardus_t%27_Hooft_at_Harvard.jpg/180px-Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://commons.wikimedia.org/wiki/File:Gerardus_t%27_Hooft_at_Harvard.jpg#mediaviewer/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
+			'https://commons.wikimedia.org/wiki/Gerardus_%27t_Hooft#mediaviewer/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
 			'https://commons.m.wikimedia.org/wiki/File:Gerardus_t%27_Hooft_at_Harvard.jpg',
 			'https://commons.m.wikimedia.org/w/index.php?title=File:Gerardus_t%27_Hooft_at_Harvard.jpg'
 		],
@@ -79,13 +79,13 @@ var testCases = [
 		}
 	}, {
 		input: [
-			'http://de.wikipedia.org/wiki/File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/wiki/File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'de.wikipedia.org/wiki/File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://de.wikipedia.org/w/index.php?title=File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://upload.wikimedia.org/wikipedia/de/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://upload.wikimedia.org/wikipedia/de/thumb/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg/320px-1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/w/index.php?title=File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://upload.wikimedia.org/wikipedia/de/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://upload.wikimedia.org/wikipedia/de/thumb/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg/320px-1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'upload.wikimedia.org/wikipedia/de/thumb/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg/320px-1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://upload.wikimedia.org/wikipedia/de/thumb/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg/320px-1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://upload.wikimedia.org/wikipedia/de/thumb/f/fb/1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg/320px-1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'https://de.m.wikipedia.org/wiki/File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'de.m.wikipedia.org/wiki/File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'https://de.m.wikipedia.org/w/index.php?title=File:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg'
@@ -96,12 +96,12 @@ var testCases = [
 		}
 	}, {
 		input: [
-			'http://de.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'de.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://de.wikipedia.org/w/index.php?title=Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/w/index.php?title=Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'de.wikipedia.org/w/index.php?title=Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://de.wikipedia.org/wiki/1._FC_Bamberg#mediaviewer/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
-			'http://de.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg#mediaviewer/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/wiki/1._FC_Bamberg#mediaviewer/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
+			'https://de.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg#mediaviewer/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'https://de.m.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'de.m.wikipedia.org/wiki/Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
 			'https://de.m.wikipedia.org/w/index.php?title=Datei:1_FC_Bamberg_-_1_FC_N%C3%BCrnberg_1901.jpg',
@@ -184,8 +184,8 @@ QUnit.test( 'getFilename() returning ImageInfo objects', function( assert ) {
 	var testCases = [
 		{
 			input: [
-				'http://en.wikipedia.org/wiki/K%C3%B6nigsberg,_Bavaria',
-				'http://en.wikipedia.org/w/index.php?title=K%C3%B6nigsberg,_Bavaria',
+				'https://en.wikipedia.org/wiki/K%C3%B6nigsberg,_Bavaria',
+				'https://en.wikipedia.org/w/index.php?title=K%C3%B6nigsberg,_Bavaria',
 				'https://en.m.wikipedia.org/wiki/K%C3%B6nigsberg,_Bavaria',
 				'https://en.m.wikipedia.org/w/index.php?title=K%C3%B6nigsberg,_Bavaria'
 
@@ -195,12 +195,12 @@ QUnit.test( 'getFilename() returning ImageInfo objects', function( assert ) {
 			}
 		}, {
 			input: [
-				'http://de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern',
-				'http://de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern?uselang=en',
+				'https://de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern',
+				'https://de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern?uselang=en',
 				'de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern',
 				'de.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern?uselang=de',
-				'http://de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern',
-				'http://de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern&uselang=de',
+				'https://de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern',
+				'https://de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern&uselang=de',
 				'de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern',
 				'de.wikipedia.org/w/index.php?title=K%C3%B6nigsberg_in_Bayern&uselang=de',
 				'https://de.m.wikipedia.org/wiki/K%C3%B6nigsberg_in_Bayern',
@@ -278,7 +278,7 @@ QUnit.test( 'getFilename returns an error when given an URL that cannot be proce
 	var testCases = [
 		'https://www.wikimedia.de/w/images.homepage/d/d6/Pavel_Richter_WMDE.JPG',
 		'https://www.wikimedia.de/w/images.homepage/d/d6/',
-		'http://foo.bar',
+		'https://foo.bar',
 		'https://de.wikipedia.org/wiki/Lars_Kindgen',
 		'https://de.m.wikipedia.org/wiki/Lars_Kindgen'
 	];
